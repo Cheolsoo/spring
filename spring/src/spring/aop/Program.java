78c2fc66e3cba1536fdee4c239681afbe9894523
cheolsoo kim <nabiro@gmail.com>
2023-11-15 11:15:56 +0900

    어느 브랜치로 가는지 궁금

--- spring/src/spring/aop/Program.java
+++ spring/src/spring/aop/Program.java
@@ -23,7 +23,7 @@ public class Program {
 		System.out.printf("total is %d\n", exam.total());
 		System.out.printf("total is %f\n", exam.avg());
 		
-		
+		// 뭔가 해서 로컬 브랜치가 dev 가 된것 같은데
 		
 		/*
 		Exam exam = new NewlecExam(1,1,1,1);
